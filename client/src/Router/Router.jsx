48833ff7960d7efd0ac311f1e343274b5edefa93
shubham-goyal-0811 <shubhamsubbi1@gmail.com>
2024-11-25 14:32:08 +0530

    client setup done

--- client/src/Router/Router.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {Routes} from "react-router-dom";
-
-const Routes =()=>{
-    return(
-        <Routes/>
-    )
-}
--- client/src/components/Layout/Layout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Routers } from "../Routers/Routers";
-const Layout=()=>{
-    return(
-        <Routers/>
-    )
-}
