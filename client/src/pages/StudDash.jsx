bfcf80a6882047c5a180a247cb034f997e52c4a1
samiksha <singhsamikshasingh2004@gmail.com>
2024-12-05 23:55:52 +0530

    adjusted student dashboard

--- client/src/pages/StudDash.jsx
+++ client/src/pages/StudDash.jsx
@@ -1,16 +1,42 @@
-import React from "react";
-import StudSideBar from "../components/StudSideBar"; // Adjust path as needed
-import Calendar from "../components/Calendar"
+import React, { useState } from "react";
+import StudSideBar from "../components/StudSideBar"; 
+import Calendar from "../components/Calendar";
+import PomodoroTimer from "../components/PomodoroTimer";
+import Schedule from "../pages/Schedule";
+import Todo from "../components/Todo";
 
 const StudDashboard = () => {
+  const [tabContent, setTabContent] = useState('StudDashboard');
+  
   return (
     <div className="flex">
-      <StudSideBar />
-      <div style={{  position: 'absolute', right: 10, top: 5,  }}>
-        <Calendar />
+      <StudSideBar setTabContent={setTabContent}/>
+      <div >
+        {tabContent === "StudDashboard" && (
+          <div >
+            <div >
+              <Calendar />
+            </div>
+            <div >
+            <Todo/>
+            </div>
+            
+          </div>
+        )}
+        {tabContent === "timer" && (
+          <div className="flex-1">
+          <PomodoroTimer />
+        </div>
+        )}
+        {tabContent==="Schedule"&&(
+          <div >
+            <Schedule/>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default StudDashboard;
+
