315eebdc4b23376b555514ba46940a4a5e8bc095
samiksha <singhsamikshasingh2004@gmail.com>
2024-12-10 11:54:54 +0530

    some adjustments

--- client/src/pages/StudDash.jsx
+++ client/src/pages/StudDash.jsx
@@ -4,6 +4,7 @@ import Calendar from "../components/Calendar";
 import PomodoroTimer from "../components/PomodoroTimer";
 import Schedule from "../pages/Schedule";
 import Todo from "../components/Todo";
+import s from '../Styles/StudDash.module.css'
 
 const StudDashboard = () => {
   const [tabContent, setTabContent] = useState('StudDashboard');
@@ -13,15 +14,14 @@ const StudDashboard = () => {
       <StudSideBar setTabContent={setTabContent}/>
       <div >
         {tabContent === "StudDashboard" && (
-          <div >
-            <div >
-              <Calendar />
-            </div>
-            <div >
-            <Todo/>
-            </div>
-            
+          <div className={s.container}>
+          <div className={s.calendar}>
+            <Calendar />
           </div>
+          <div className={s.todo}>
+            <Todo/>
+          </div>
+        </div>
         )}
         {tabContent === "timer" && (
           <div className="flex-1">
